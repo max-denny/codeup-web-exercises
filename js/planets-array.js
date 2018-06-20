99361d36ee653b50b0fb3c10b997b716e2603e8f
Robin Maxwell <robinmaxwell@Robin-Maxwells-MacBook-Pro.local>
2018-06-20 00:56:20 -0400

    Completion Commit .. Manipulating Arrays Exercise .. planets-array.js

--- js/planets-array.js
+++ js/planets-array.js
@@ -19,22 +19,35 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift('The Sun');
     console.log(planets);
 
+
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push('Pluto');
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift([0]);
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop('Pluto');
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    var indexEarth = planets.indexOf('Earth');
+    console.log('The index of the planet Earth is: ' + indexEarth);
 
     console.log("Reversing the order of the planets array.");
+
+    planets.reverse();
+
     console.log(planets);
 
     console.log("Sorting the planets array.");
+
+    planets.sort();
+
     console.log(planets);
 })();
